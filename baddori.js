693d75c0be4ce44954a4d8c7e12990c7f462a65c
malgantual <56133144+malgantual@users.noreply.github.com>
2021-07-23 19:29:37 -0500

    did some things lol

--- baddori.js
+++ baddori.js
@@ -1,10 +1,65 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
-var img = new Image();
-img.onload = function() {
+// variables
+var notew = 100, noteh = 30
+var l1 = (canvas.width - (notew * 6)) / 2, l2 = l1 + notew, l3 = l2 + notew, l4 = l3 + notew, l5 = l4 + note; //, LANE2 = LANE1 + notew, LANE3 = LANE2 + notew, LANE4 = LANE3 + notew, LANE5 = LANE4 + notew;
+var note = {x:l1, y:0}
+
+var canvasbg = new Image()
+canvasbg.src = 'yukina2.png';
+
+var notespeed = 3
+
+setup()
+
+function setup()
+{
+	// yukina();
+}
+
+function drawCanvasbg()
+{
 	ctx.globalAlpha = 0.5;
-	ctx.drawImage(img, 0, 0);
+	ctx.drawImage(canvasbg, 0, 0);
 	ctx.globalAlpha = 1;
-};
-img.src = 'yukina.png';
+}
+
+function drawNotes()
+{
+	ctx.beginPath();
+    ctx.rect(note.x, note.y, notew, noteh);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+	note.y += notespeed;
+}
+
+function draw()
+{
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawCanvasbg();
+	drawNotes();
+}
+setInterval(draw, 10)
+
+function collisionCheck(coords)
+{
+
+}
+
+function getCursorPosition(canvas, event)
+{
+    var rect = canvas.getBoundingClientRect()
+    var x = event.clientX - rect.left
+    var y = event.clientY - rect.top
+    console.log("x: " + x + " y: " + y)
+	return {x: x, y: y}
+}
+
+c.addEventListener('mousedown', function(e)
+{
+    coords = getCursorPosition(canvas, e)
+	collisionCheck(coords)
+})
